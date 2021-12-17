cfd1f77983fd16f3e4fe3934b714eeb0d05688d0
Mike Clease <clease.m@gmail.com>
2021-12-17 22:04:17 +0000

    Fixed Bandname print

--- Day 1. BandNameGenerator.py
+++ Day 1. BandNameGenerator.py
@@ -2,4 +2,4 @@ print("So you're struggling with a band name.")
 name1 = input("What city did you grow up in? \n")
 name2 = input("What was the name of your first pet? \n")
 band_name = name1 + " " + name2
-print("Based on your answers your band name could be "+band_name".")
+print(f"Based on your answers your band name could be {band_name}.")
