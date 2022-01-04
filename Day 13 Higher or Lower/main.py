d1a449c21e81b09e1008d67cd567e4f8bf9f5f42
Mike Clease <m.clease@techmodal.com>
2022-01-04 10:31:45 +0000

    tidied up

--- Day 13 Higher or Lower/main.py
+++ Day 13 Higher or Lower/main.py
@@ -1,19 +1,25 @@
-#plan
+# plan
 # data is a list of dicts, so pick 2 random items from list and have A vs B
 # "list[0][name], a list[0][desription] from list[0][country]"
-# nicked a 
+# restructured some of it based on published solution, some functions are just
+# a bit better than what I'd written
 
 import random
 from game_data import data
 from art import logo, vs
 import os
 
-clear = lambda: os.system('cls')
+
+def clear():
+    os.system('cls')
+
 
 print(logo)
 
+
 def make_choice(data):
     return random.choice(data)
+
 
 def format_choice(choice):
     name = choice['name']
@@ -21,12 +27,14 @@ def format_choice(choice):
     country = choice['country']
     return f"{name}, a {description} from {country}."
 
+
 def compare(a, b, guess):
     if a['follower_count'] > b['follower_count']:
         return guess == 'a'
     else:
         return guess == 'b'
- 
+
+
 def game():
     a = make_choice(data)
     b = make_choice(data)
@@ -56,24 +64,5 @@ def game():
             print("Wrong! Game Over! Your final score is: {score}")
             continue_game = False
 
+
 game()
-
-# print(f"COMPARE A: {vs[0]['name']}, a {vs[0]['description']} from {vs[0]['country']}")
-# print(vs)
-# print(f"AGAINST B: {vs[1]['name']}, a {vs[1]['description']} from {vs[1]['country']}")
-
-# # needs to check for valid input
-# if input("Who has more followers A or B? ").to_upper() == 'A':
-#     choice = 0
-#     other = 1
-# else:
-#     other = 0
-#     choice = 1
-
-# # keep going until get one wrong, count score in the meantime.
-# if vs[choice]['follower_count'] > vs[other]['follower_count']:
-#     print("Correct")
-# else:
-#     print("Wrong")
-
-
