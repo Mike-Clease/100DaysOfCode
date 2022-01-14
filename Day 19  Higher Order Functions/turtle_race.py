386ad7a2b803dcbdcb12973d754eb910956bda06
Mike Clease <clease.m@gmail.com>
2022-01-14 19:47:51 +0000

    Day 19 - thought I'd fixed this...

--- Day 19  Higher Order Functions/turtle_race.py
+++ Day 19  Higher Order Functions/turtle_race.py
@@ -4,42 +4,23 @@ import random
 
 def step(ttl):
     ttl.forward(random.randint(0, 10))
-    
-    
-# class racer(Turtle):
-    
-#     def __init__(self, x_pos, y_pos, name, colour):
-#         self.position = self.goto(x_pos, y_pos)
-#         self.name = name
-#         self.colour = colour
-#         self.shape = self.shape('Turtle')
+
 
 scn = Screen()
 scn.setup(width=500, height=400)
 
-red = Turtle(shape='turtle')
-green = Turtle(shape='turtle')
-blue = Turtle(shape='turtle')
-purple = Turtle(shape='turtle')
-yellow = Turtle(shape='turtle')
-orange = Turtle(shape='turtle')
+colour_list = ['red', 'green', 'blue', 'purple', 'yellow', 'orange']
 
-red.penup()
-green.penup()
-blue.penup()
-purple.penup()
-yellow.penup()
-orange.penup()
+start_y = -75
 
-red.goto(-220, 15)
-green.goto(-220, 45)
-blue.goto(-220, 75)
-purple.goto(-220, -15)
-yellow.goto(-220, -45)
-orange.goto(-220, -75)
+for colour in colour_list:
+    ttl = Turtle(shape='turtle')
+    ttl.color(colour)
+    ttl.penup()
+    ttl.goto(-250, start_y)
+    start_y += 30
 
-
-
-bet = scn.textinput(title='Make your bet', prompt='Which turtle will win the race?')
+bet = scn.textinput(title='Make your bet',
+                    prompt='Which turtle will win the race?')
 
 scn.exitonclick()
