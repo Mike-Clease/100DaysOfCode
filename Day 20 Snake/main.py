1cd0594a908570bb9e48c02cd042959a9b8987b8
Mike Clease <clease.m@gmail.com>
2022-01-16 16:24:21 +0000

    Finished day 20

--- Day 20 Snake/main.py
+++ Day 20 Snake/main.py
@@ -31,10 +31,20 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         board.increase_score()
+        snake.grow()
 
     # Detech wall collision
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        board.game_over()
+    # if snake.head.xcor() > 300 or snake.head.xcor() < -300 \
+    #     or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+    #     game_is_on = False
+    #     board.game_over()
+
+    # Detect tail collision
+    # if snake.head.position() game over
+    for segment in snake.segments[1:]:
+        dist = abs(snake.head.distance(segment))
+        if dist < 10:
+            game_is_on = False
+            board.game_over()
 
 scn.exitonclick()
--- Day 20 Snake/snake.py
+++ Day 20 Snake/snake.py
@@ -1,7 +1,7 @@
 from pickletools import UP_TO_NEWLINE
 from turtle import Turtle
 
-START_POS = [(-20, 0), (0, 0), (20, 0)]
+START_POS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DIST = 20
 UP = 90
 DOWN = 270
@@ -9,20 +9,17 @@ RIGHT = 0
 LEFT = 180
 
 
-class Snake:
+class Snake(Turtle):
 
     def __init__(self):
+        super().__init__()
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
 
     def create_snake(self):
         for pos in START_POS:
-            segment = Turtle(shape='square')
-            segment.penup()
-            segment.goto(pos)
-            segment.color('white')
-            self.segments.append(segment)
+            self.add_segment(pos)
 
     def move(self):
         for seg in range(len(self.segments)-1, 0, -1):
@@ -34,23 +31,39 @@ class Snake:
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+            self.move()
         else:
             pass
 
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
+            self.move()
         else:
             pass
 
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
+            self.move()
         else:
             pass
 
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+            self.move()
         else:
             pass
+
+    def add_segment(self, position):
+        segment = Turtle(shape='square')
+        segment.penup()
+        segment.goto(position)
+        segment.color('white')
+        self.segments.append(segment)
+
+    def grow(self):
+        # add new segment to the snake
+        last_seg = self.segments[-1].position()
+        self.add_segment(last_seg)   
