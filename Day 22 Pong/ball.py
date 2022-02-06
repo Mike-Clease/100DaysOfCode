91514ffc68d3d64e4fe3d21cc769dc391ea5b753
Mike Clease <clease.m@gmail.com>
2022-02-06 17:48:06 +0000

    Removing checks

--- Day 22 Pong/ball.py
+++ Day 22 Pong/ball.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 
+# Tried to get the bouncing based off of headings rather than coding a move
+# like was done in the session, it would have been pretty codey to do
+# so ended up just going with the way it's done online :-(
+
 
 class Ball(Turtle):
 
@@ -10,19 +14,19 @@ class Ball(Turtle):
         self.penup()
         self.shapesize(stretch_len=1, stretch_wid=1)
         self.setheading(45)
+        self.x_move = 5
+        self.y_move = 5
 
     def move(self):
-        self.forward(10)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
     def bounce(self):
-        self.setheading(self.heading() * -1)
+        self.y_move *= -1
 
     def ricochet(self):
-        if self.heading() <= 90:
-            self.setheading(90 + self.heading())
-        elif self.heading() <= 180:
-            self.setheading(90 - (self.heading() - 90))
-        elif self.heading() <= 270:
-            self.setheading((270 - self.heading()) + 180)
-        else:
-            self.setheading(270 - (360 - self.heading()))
+        self.x_move *= -1
+
+    def reset(self):
+        self.goto(0, 0)
